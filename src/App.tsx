311751d39cc1f9924385f6d8c4d02981435b2e0c
TheMaty <atalay1977@hotmail.com>
2020-09-30 17:31:52 +0100

    massive checkedin
    massive check in

--- src/App.tsx
+++ src/App.tsx
@@ -55,7 +55,7 @@ function App() {
 
   // Create async function for fetching customer list
   const fetchCustomer = async () => {
-    const customerFetched = await fetch('/api/download?filename=asset.json')
+    const customerFetched = await fetch('/api/download?filename=DavidCooperfield.json')
       .then(res => res.json()) // Process the incoming data
     // Update customerList state
     setCustomer(customerFetched)
@@ -66,6 +66,8 @@ function App() {
         {/* Button to fetch customer data */}
         <button onClick={fetchCustomer}>Fetch Customer</button>
         {/* Display table of customer after fetching customer demographic information data */}
+        <br/>
+        <b>Customer Information</b>
         {customer !== null && customer !== undefined && <table>
           <thead>
             <tr>
@@ -73,6 +75,10 @@ function App() {
               <th>Last Name</th>
               <th>EMail</th>
               <th>National Id</th>
+              <th>Created Date</th>
+              <th>Type</th>
+              <th>License</th>
+              <th>Price</th>
             </tr>
           </thead>
           <tbody>
@@ -82,6 +88,54 @@ function App() {
                 <td>{customer.customerCard.main.lastname}</td>
                 <td>{customer.customerCard.main.email}</td>
                 <td>{customer.customerCard.main.nationalid}</td>
+                <td>{customer.customerCard.main.dateCreated}</td>
+                <td>{customer.customerCard.main.type}</td>
+                <td>{customer.customerCard.main.license}</td>
+                <td>{customer.customerCard.main.price}</td>
+              </tr>
+
+          </tbody>
+        </table>}
+        <br/>
+        <b>Customer Activities</b>
+        {customer !== null && customer !== undefined && <table>
+          <thead>
+            <tr>
+              <th>Type</th>
+              <th>Date</th>
+              <th>From</th>
+              <th>To</th>
+            </tr>
+          </thead>
+          <tbody>
+            
+              <tr key={customer.customerCard.additionalInformation.activities[0].type}>
+                <td>{customer.customerCard.additionalInformation.activities[0].type}</td>
+                <td>{customer.customerCard.additionalInformation.activities[0].dateCreated}</td>
+                <td>{customer.customerCard.additionalInformation.activities[0].from}</td>
+                <td>{customer.customerCard.additionalInformation.activities[0].to}</td>
+              </tr>
+
+          </tbody>
+        </table>}
+        <br/>
+        <b>Customer Activities</b>
+        {customer !== null && customer !== undefined && <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Date</th>
+              <th>Subscription</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            
+              <tr key={customer.customerCard.additionalInformation.incidents[0].name}>
+                <td>{customer.customerCard.additionalInformation.incidents[0].name}</td>
+                <td>{customer.customerCard.additionalInformation.incidents[0].dateCreated}</td>
+                <td>{customer.customerCard.additionalInformation.incidents[0].subscription}</td>
+                <td>{customer.customerCard.additionalInformation.incidents[0].status}</td>
               </tr>
 
           </tbody>
